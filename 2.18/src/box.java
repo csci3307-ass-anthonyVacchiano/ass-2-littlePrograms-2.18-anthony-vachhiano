a3dd5e85ce586479e6dd456403d41d284b96dd04
anthony <anthony@132.238.235.82>
2017-05-13 12:17:59 -0400

    readme

---
+++ 2.18/src/box.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class box {
+	public static void main(String[] arg)
+	{
+		System.out.println("*****       **       *           *     ");
+		System.out.println("*   *     *    *    ***         *  *   ");
+		System.out.println("*   *    *      *  *****      *      * ");
+		System.out.println("*   *    *      *    *        *      *   ");
+		System.out.println("*   *    *      *    *        *     *    ");
+		System.out.println("*   *    *      *    *         *   *    ");
+		System.out.println("*   *     *    *     *          * *     ");
+		System.out.println("*****       **       *           *    ");
+	}
+}
